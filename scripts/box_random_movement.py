d2b3da332e0d585a7050f2028e18fc1ceb15ed88
surue <nicolas06062000@hotmail.com>
2018-04-16 12:09:49 +0200

    update scrpit to make it more efficient

--- scripts/box_random_movement.py
+++ scripts/box_random_movement.py
@@ -4,19 +4,18 @@ import random
 
 class BoxRandomMovement(Component):
     def init(self):
-        d = p2Vec2(random.uniform(0, 0.5) - 0.25, random.uniform(0, 0.5) - 0.25)
         self.body = self.game_object.get_component(Component.Body)
-        self.body.velocity = d
+        self.originalVelocity = p2Vec2(random.uniform(0, 0.5) - 0.25, random.uniform(0, 0.5) - 0.25)
+        self.body.velocity = self.originalVelocity
 
     def update(self, dt):
-        d = self.body.velocity
         position = self.game_object.transform.get_position()
         if position.x < 0 or position.x > 1280:
-            d.x = d.x * -1
-            self.body.velocity = d
+            self.originalVelocity.x = self.originalVelocity.x * -1
+            self.body.velocity = self.originalVelocity
         if position.y < 0 or position.y > 720:
-            d.y = d.y * -1
-            self.body.velocity = d
+            self.originalVelocity.y = self.originalVelocity.y * -1
+            self.body.velocity = self.originalVelocity
 
     def on_collision_enter(self, collider):
         pass
