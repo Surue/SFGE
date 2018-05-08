b3a57e90bd89bc45b907493dc4025387525f77f9
surue <nicolas06062000@hotmail.com>
2018-05-08 07:53:48 +0200

    -fix script to raycast with mouse as position

--- scripts/raycast_star.py
+++ scripts/raycast_star.py
@@ -5,18 +5,18 @@ class RaycastStar(Component):
     def init(self):
         self.max_distance = 1000
 
-        self.nbRaycast = 20;
+        self.nbRaycast = 50;
 
         self.directions = [Vector2f()] * self.nbRaycast
+
+        self.dir = Vector2f(1, 0);
 
         ratio = 360 / self.nbRaycast
 
         angle = 0;
 
         for i in range(self.nbRaycast):
-            self.directions[i] = RaycastStar.rotation(self, i * ratio)
-
-
+            self.directions[i] = RaycastStar.rotation(self, ratio)
 
     def update(self, dt):
         mousePosition = input_manager.mouse.local_position(graphic_manager.get_window())
@@ -25,14 +25,14 @@ class RaycastStar(Component):
         for i in range(self.nbRaycast):
             physic_manager.raycast(self.directions[i], position, self.max_distance)
 
-
     def rotation(self, angle):
-        print(angle)
         angle = radians(angle)
 
-        direction = Vector2f(1, 0);
+        direction = self.dir;
 
         direction.x = (cos(angle) * direction.x) - (sin(angle) * direction.y)
         direction.y = (sin(angle) * direction.x) + (cos(angle) * direction.y)
+
+        self.dir = Vector2f(direction.x, direction.y)
 
         return direction
